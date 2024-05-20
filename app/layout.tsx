4e9782b22db67aea51207e8e382351e6ea8661da
Paul Dec <paul.decrosse@gmail.com>
2024-05-20 16:32:07 +0200

    add all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: './Ramona-Bold.ttf' })
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,47 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
+  let pos = 1;
+  function right() {
+    document.getElementById(pos.toString())?.setAttribute("style", "display: none");
+    pos++;
+    if (pos > 6) {
+      pos = 1;
+    }
+    document.getElementById(pos.toString())?.setAttribute("style", "display: flex");
+  }
+  function left() {
+    document.getElementById(pos.toString())?.setAttribute("style", "display: none");
+    pos--;
+    if (pos < 1) {
+      pos = 6;
+    }
+    document.getElementById(pos.toString())?.setAttribute("style", "display: flex");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center px-2 py-8 bg-[#14412D]">
+      <div className="flex flex-col z-10 w-full max-w-xl items-center justify-between">
+        <h1 className="flex text-center text-[#FFDE18] text-4xl w-full mb-6">Fini les pates au beurre, j’ai sorti mon livre</h1>
+        <div className="flex w-[260px] min-h-[370px] justify-center mb-6 relative">
+          <Image id="1" alt="livre 1" src={"/livre_1.png"} width={1000} height={1000} className="flex w-[260px] max-w-sm rounded-xl" />
+          <Image id="2" alt="livre 2" src={"/livre_2.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="3" alt="livre 3" src={"/livre_3.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="4" alt="livre 4" src={"/livre_4.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="5" alt="livre 5" src={"/livre_5.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="6" alt="livre 6" src={"/livre_6.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <svg onClick={right} className="absolute left-full top-1/2 -translate-y-1/2" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M26.2487 24.9998L18.1237 16.8748C17.7418 16.4929 17.5508 16.0068 17.5508 15.4165C17.5508 14.8262 17.7418 14.3401 18.1237 13.9582C18.5056 13.5762 18.9918 13.3853 19.582 13.3853C20.1723 13.3853 20.6584 13.5762 21.0404 13.9582L30.6237 23.5415C30.832 23.7498 30.9796 23.9755 31.0664 24.2186C31.1532 24.4616 31.1966 24.7221 31.1966 24.9998C31.1966 25.2776 31.1532 25.538 31.0664 25.7811C30.9796 26.0241 30.832 26.2498 30.6237 26.4582L21.0404 36.0415C20.6584 36.4234 20.1723 36.6144 19.582 36.6144C18.9918 36.6144 18.5056 36.4234 18.1237 36.0415C17.7418 35.6596 17.5508 35.1734 17.5508 34.5832C17.5508 33.9929 17.7418 33.5068 18.1237 33.1248L26.2487 24.9998Z" fill="#FFDE18" />
+          </svg>
+          <svg onClick={left} className="absolute right-full top-1/2 -translate-y-1/2" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M23.7513 24.9998L31.8763 16.8748C32.2582 16.4929 32.4492 16.0068 32.4492 15.4165C32.4492 14.8262 32.2582 14.3401 31.8763 13.9582C31.4944 13.5762 31.0082 13.3853 30.418 13.3853C29.8277 13.3853 29.3416 13.5762 28.9596 13.9582L19.3763 23.5415C19.168 23.7498 19.0204 23.9755 18.9336 24.2186C18.8468 24.4616 18.8034 24.7221 18.8034 24.9998C18.8034 25.2776 18.8468 25.538 18.9336 25.7811C19.0204 26.0241 19.168 26.2498 19.3763 26.4582L28.9596 36.0415C29.3416 36.4234 29.8277 36.6144 30.418 36.6144C31.0082 36.6144 31.4944 36.4234 31.8763 36.0415C32.2582 35.6596 32.4492 35.1734 32.4492 34.5832C32.4492 33.9929 32.2582 33.5068 31.8763 33.1248L23.7513 24.9998Z" fill="#FFDE18" />
+          </svg>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            RORO LA BOSS
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <a href="https://www.amazon.fr/Japprends-%C3%A0-cuisiner-avec-Roro/dp/2017239844" target="_blank" className="flex text-center w-full max-w-[360px] justify-center py-4 bg-[#FFDE18] text-[#14412D] font-bold rounded-xl  text-xl m-3">Précommander sur Amazon</a>
+        <a href="https://www.fnac.com/a19419266/Aurore-Ghigliano-J-apprends-a-cuisiner-avec-Roro" target="_blank" className="flex text-center w-full max-w-[360px] justify-center py-4 bg-[#FFDE18] text-[#14412D] font-bold rounded-xl  text-xl m-3">Précommander sur la Fnac</a>
+        <a href="https://www.leslibraires.fr/livre/23359981-j-apprends-a-cuisiner-avec-roro-aurore-ghigliano-hachette-pratique" target="_blank" className="flex text-center w-full max-w-[360px] justify-center py-4 bg-[#FFDE18] text-[#14412D] font-bold rounded-xl  text-xl m-3">Précommander sur Leslibraires</a>
       </div>
     </main>
   );
