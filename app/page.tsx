85ea9b00eb183783b70d8d79384309695c0b60c6
Paul Dec <paul.decrosse@gmail.com>
2024-05-20 16:37:55 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -26,12 +26,12 @@ export default function Home() {
       <div className="flex flex-col z-10 w-full max-w-xl items-center justify-between">
         <h1 className="flex text-center text-[#FFDE18] text-4xl w-full mb-6">Fini les pates au beurre, j’ai sorti mon livre</h1>
         <div className="flex w-[260px] min-h-[370px] justify-center mb-6 relative">
-          <Image id="1" alt="livre 1" src={"/livre_1.png"} width={1000} height={1000} className="flex w-[260px] max-w-sm rounded-xl" />
-          <Image id="2" alt="livre 2" src={"/livre_2.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
-          <Image id="3" alt="livre 3" src={"/livre_3.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
-          <Image id="4" alt="livre 4" src={"/livre_4.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
-          <Image id="5" alt="livre 5" src={"/livre_5.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
-          <Image id="6" alt="livre 6" src={"/livre_6.png"} width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="1" alt="livre 1" src={"/livre_1.png"} priority width={1000} height={1000} className="flex w-[260px] max-w-sm rounded-xl" />
+          <Image id="2" alt="livre 2" src={"/livre_2.png"} priority width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="3" alt="livre 3" src={"/livre_3.png"} priority width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="4" alt="livre 4" src={"/livre_4.png"} priority width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="5" alt="livre 5" src={"/livre_5.png"} priority width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
+          <Image id="6" alt="livre 6" src={"/livre_6.png"} priority width={1000} height={1000} className="hidden w-[260px] max-w-sm rounded-xl" />
           <svg onClick={right} className="absolute left-full top-1/2 -translate-y-1/2" width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M26.2487 24.9998L18.1237 16.8748C17.7418 16.4929 17.5508 16.0068 17.5508 15.4165C17.5508 14.8262 17.7418 14.3401 18.1237 13.9582C18.5056 13.5762 18.9918 13.3853 19.582 13.3853C20.1723 13.3853 20.6584 13.5762 21.0404 13.9582L30.6237 23.5415C30.832 23.7498 30.9796 23.9755 31.0664 24.2186C31.1532 24.4616 31.1966 24.7221 31.1966 24.9998C31.1966 25.2776 31.1532 25.538 31.0664 25.7811C30.9796 26.0241 30.832 26.2498 30.6237 26.4582L21.0404 36.0415C20.6584 36.4234 20.1723 36.6144 19.582 36.6144C18.9918 36.6144 18.5056 36.4234 18.1237 36.0415C17.7418 35.6596 17.5508 35.1734 17.5508 34.5832C17.5508 33.9929 17.7418 33.5068 18.1237 33.1248L26.2487 24.9998Z" fill="#FFDE18" />
           </svg>
